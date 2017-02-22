20e8ae80118db46947cd62974bc421456f975b5a
ALEX MARTINEZ <martineza79@outlook.com>
2017-02-22 14:52:09 -0600

    Add project files.

---
+++ WebAPIRESTServices/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIRESTServices.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [HttpPost]
+        public bool AddEmpDetails()
+        {
+            return true;
+        }
+
+        [HttpGet]
+        public string GetEmpDetails()
+        {
+            return "Willie Wanka";
+        }
+
+        [HttpDelete]
+        public string DeleteEmpDetails(string id)
+        {
+            return "Employees details delete having Id " + id;
+        }
+
+        [HttpPut]
+        public string UpdateEmpDetails(string Name, String Id)
+        {
+            return "Employee details Updated with Name " + Name + " and id " + Id;
+        }
+    }
+}
